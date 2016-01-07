4098774cd8074cc8423792ad7459466d0263abcb
Yaro Beefeater <12fcv4@gmail.com>
2016-01-07 23:56:26 +0200

    Update wo.js

---
+++ wo.js
@@ -0,0 +1,59 @@
+window.wo = (function() {
+    'use strict';
+
+    var wo = 
+    {
+        template: null,
+        data: [],
+        
+        fetch: function(template, data)
+        {
+            this.template = this.template || document.getElementById(template);
+            data = data || {};
+            
+            var html = this.template.text;
+            
+            return html.replace(/(~([^~]+)~)/g, function(match, contents, offset, s) {
+                var res = null;
+                offset.split('.').forEach(function(item) {
+                    var tmp = (res === null) ? data : res;
+                    res = typeof tmp[item] === 'undefined' ? false : tmp[item];
+                });
+                
+                return res || match;
+            });
+        }, // end fetch
+        
+        each: function(data)
+        {
+            this.data = data || [];
+            
+            return this;
+        }, // end each
+        
+        render: function(template, data)
+        {
+            var self = this;
+            
+            this.template = this.template || document.getElementById(template);
+            this.data = data || this.data;
+            if (!Array.isArray(this.data)) {
+                this.data = [this.data];
+            }
+            
+            var html = '';
+            this.data.forEach(function(item) {
+                html += self.fetch(template, item);
+            });
+            
+            var into = this.template.getAttribute('into');
+            if (into) {
+                return document.getElementById(into).innerHTML = html;
+            }
+            return html;
+        }, // end render
+        
+    };
+     
+    return wo;
+}());
